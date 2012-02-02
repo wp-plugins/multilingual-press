ce1f816d84f9b81049f6678cc7c4dd6786895830
Bueltge <Bueltge@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-02 11:06:37 +0000

    Update Trunk, new alpha release
    git-svn-id: https://plugins.svn.wordpress.org/multilingual-press/trunk@499073 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- inc/class-mlp-custom-columns.php
+++ inc/class-mlp-custom-columns.php
@@ -1,15 +1,28 @@
 <?php
 /**
- * Multilingual Press custom columns class
- * Credits to http://wordpress.stackexchange.com/users/2487/bainternet
+ * MultilingualPress custom columns class
  * 
- * @version 1.0
+ * @version 0.5.1a
+ * @since   0.5.0
+ */
+
+/**
+ * Changelog
+ * 
+ * 0.5a
+ * - added filters for module support
  * 
  */
-class mlp_custom_columns extends inpsyde_multilingualpress {
+class Mlp_Custom_Columns extends Inpsyde_Multilingualpress {
 
 	static protected $class_object = NULL;
-
+	
+	/**
+	 * Init class
+	 * 
+	 * @since  0.1
+	 * @return object
+	 */
 	public static function init() {
 
 		if ( NULL == self::$class_object ) {
@@ -17,49 +30,90 @@ class mlp_custom_columns extends inpsyde_multilingualpress {
 		}
 		return self::$class_object;
 	}
-
+	
+	/**
+	 * Constructor
+	 * Init methods in WP
+	 * 
+	 * @since  0.1
+	 * @return void
+	 */
 	public function __construct() {
 
 		add_filter( 'wpmu_blogs_columns', array( $this, 'get_id' ) );
 		add_action( 'manage_sites_custom_column', array( $this, 'add_columns' ), 10, 2 );
-		add_action( 'manage_blogs_custom_column', array( $this, 'add_columns' ), 10, 2 );
-		//add_action( 'admin_footer', array( $this, 'add_style' ) );
+		//add_action( 'manage_blogs_custom_column', array( $this, 'add_columns' ), 10, 2 );
 	}
-
+	
+	/**
+	 * Add colums to blog view
+	 * 
+	 * @since  0.1
+	 * @param  $column_name  string
+	 * @param  $blog_id      integer
+	 * @return $column_name  string
+	 */
 	public function add_columns( $column_name, $blog_id ) {
 
 		//render column value
 		if ( 'mlp_interlinked' === $column_name ) {
+					
 			switch_to_blog( $blog_id );
+			
 			$interlinked = mlp_get_available_languages_titles();
-			if ( ! is_array( $interlinked ) ) return;
+			
+			if ( ! is_array( $interlinked ) || 
+				 ( isset( $interlinked[4] ) && '-1' === $interlinked[4] ) ) {
+				_e( 'nothing', $this->get_textdomain() );
+				return;
+			}
+			
 			$url = network_admin_url();
 			
+			echo '<div class="mlp_interlinked_blogs">';
 			foreach( $interlinked AS $interlinked_blog_id => $interlinked_blog_title ) {
 				
-				if ( $interlinked_blog_id == $blog_id ) continue;
+				if ( $interlinked_blog_id == $blog_id )
+					continue;
 				
-				echo "<img src=\"" . mlp_get_language_flag( $interlinked_blog_id ) . "\" alt=\"{$interlinked_blog_title}\" />&nbsp;<a href=\"{$url}site-info.php?id={$blog_id}\">" . $interlinked_blog_title . "</a><br />";
-				
+				//echo "<img src=\"" . mlp_get_language_flag( $interlinked_blog_id ) 
+				//	. "\" alt=\"{$interlinked_blog_title}\" />&nbsp;" 
+				//	. $interlinked_blog_title . "<br />";
+				echo '<img src="' 
+					. mlp_get_language_flag( $interlinked_blog_id ) 
+					. '" alt="' . $interlinked_blog_title 
+					. '" title="' . $interlinked_blog_title 
+					. '" class="mlp_interlinked_blog_' . $interlinked_blog_id
+					. '" width="16" height="11"'
+					. '/>&nbsp;';
 			}
-			//echo get_blog_option( $blog_id, 'blog_expire', "Default Value To Show if none" );
+			echo '</div>';
+			
+			restore_current_blog();
 		}
+				
+		$column_name = apply_filters( 'mlp_add_custom_columns', $column_name, $blog_id );
+		
 		return $column_name;
 	}
 
-	// Add in a column header
+	/**
+	 * Add in a column header
+	 * Use Filter Hook mlp_add_custom_columns_header for custom string
+	 * 
+	 * @since   0.7.1a
+	 * @param   $columns  Array
+	 * @return  $columns  Array
+	 */
 	public function get_id( $columns ) {
-
+		
 		//add extra header to table
 		$columns[ 'mlp_interlinked' ] = __( 'Interlinked with', $this->get_textdomain() );
-
+		
+		$columns = apply_filters( 'mlp_add_custom_columns_header', $columns );
+		
 		return $columns;
 	}
-
-	public function add_style() {
-
-		//echo '<style>#blog_id { width:7%; }</style>';
-	}
-
-}
+	
+} // end class
 ?>
--- inc/class-mlp-widget.php
+++ inc/class-mlp-widget.php
@@ -1,12 +1,19 @@
 <?php
 /**
  * Multilingual Press widget class
- * Version: 1.1b
+ * Version: 0.7a
  * 
  */
-if ( ! class_exists( 'mlp_widget' ) ) {
 
-	class mlp_widget extends WP_Widget {
+/**
+ * Changelog
+ * 
+ * 0.7a
+ * - Use URL-parameter to avoid redirection 
+ */
+if ( ! class_exists( 'Mlp_Widget' ) ) {
+
+	class Mlp_Widget extends WP_Widget {
 
 		private $textdomain = '';
 
@@ -14,16 +21,16 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 		 * Widget init
 		 * 
 		 */
-		function mlp_widget() {
+		public function mlp_widget() {
 			
 			$this->textdomain = inpsyde_multilingualpress :: get_textdomain();
 			
 			$widget_ops = array(
-				'classname' => 'mlp_widget',
+				'classname' => 'Mlp_Widget',
 				'description' => __( 'Multilingual Press Widget', $this->textdomain )
 			);
 			
-			$this->WP_Widget( 'mlp_widget', __( 'Multilingual Press Widget', $this->textdomain ), $widget_ops );
+			$this->WP_Widget( 'Mlp_Widget', __( 'Multilingual Press Widget', $this->textdomain ), $widget_ops );
 		}
 
 		/**
@@ -31,9 +38,9 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 		 * 
 		 * @param array $instance | widget settings 
 		 */
-		function form( $instance ) {
+		public function form( $instance ) {
 
-			$title = ( ISSET( $instance[ 'widget_title' ] ) ) ? esc_attr( $instance[ 'widget_title' ] ) : '';
+			$title = ( ISSET( $instance[ 'widget_title' ] ) ) ? strip_tags( $instance[ 'widget_title' ] ) : '';
 			$link_type = ( ISSET( $instance[ 'widget_title' ] ) ) ? esc_attr( $instance[ 'widget_link_type' ] ) : '';
 			?>
 			<p>
@@ -43,12 +50,9 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 			<p>
 				<label for='<?php echo $this->get_field_id( "mlp_widget_link_type" ); ?>'><?php _e( 'Link-Type:', $this->textdomain ); ?></label><br />	
 				<select class="widefat" id='<?php echo $this->get_field_id( "mlp_widget_link_type" ); ?>' name='<?php echo $this->get_field_name( "mlp_widget_link_type" ); ?>' >
-					<option <?php if ( 'text' === $link_type )
-				echo 'selected="selected"'; ?> value='text'><?php _e( 'Text', $this->textdomain ); ?></option>
-					<option <?php if ( 'flag' === $link_type )
-				echo 'selected="selected"'; ?> value='flag'><?php _e( 'Flag', $this->textdomain ); ?></option>
-					<option <?php if ( 'lang_code' === $link_type )
-				echo 'selected="selected"'; ?> value='lang_code'><?php _e( 'Language code', $this->textdomain ); ?></option>
+					<option <?php selected( $link_type, 'text' ); ?> value="text"><?php _e( 'Text', $this->textdomain ); ?></option>
+					<option <?php selected( $link_type, 'flag' ); ?> value="flag"><?php _e( 'Flag', $this->textdomain ); ?></option>
+					<option <?php selected( $link_type, 'lang_code' ); ?> value="lang_code"><?php _e( 'Language code', $this->textdomain ); ?></option>
 				</select>
 			</p>
 			<?php
@@ -60,10 +64,10 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 		 * @param array $instance | widget settings 
 		 * @param array $new_instance | new widget settings 
 		 */
-		function update( $new_instance, $old_instance ) {
+		public function update( $new_instance, $old_instance ) {
 
 			$instance = $old_instance;
-			$instance[ 'widget_title' ] = esc_attr( $new_instance[ 'mlp_widget_title' ] );
+			$instance[ 'widget_title' ] = strip_tags( $new_instance[ 'mlp_widget_title' ] );
 			$instance[ 'widget_link_type' ] = esc_attr( $new_instance[ 'mlp_widget_link_type' ] );
 
 			return $instance;
@@ -76,11 +80,11 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 		 * @param array $instance | widget settings
 		 * @return void
 		 */
-		function widget( $args, $instance ) {
+		public function widget( $args, $instance ) {
 			
 			extract( $args );
 			
-			$languages = mlp_get_available_languages();
+			$languages = mlp_get_available_languages( TRUE );
 			$language_titles = mlp_get_available_languages_titles();
 			
 			if ( ! ( 0 < count( $languages ) ) )
@@ -98,21 +102,20 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 			echo '<ul>';
 
 			foreach ( $languages as $language_blog => $language_string ) {
-
+				
 				// Get params
 				$flag = mlp_get_language_flag( $language_blog );
-				$title = mlp_get_available_languages_titles();
+				$title = mlp_get_available_languages_titles( TRUE );
 				
 				// Display type
-				if( 'flag' == $instance[ 'widget_link_type' ] && '' != $flag ) {
+				if ( 'flag' == $instance[ 'widget_link_type' ] && '' != $flag ) {
 					
 					$display = '<img src="' . $flag . '" alt="' . $languages[ $language_blog ] . '" title="' . $title[ $language_blog ] . '" />';
 				}
-				elseif ( 'text' == $instance[ 'widget_link_type' ] && '' != $language_titles[ $language_blog ] ) {
-					
+				else if ( 'text' == $instance[ 'widget_link_type' ] && ! empty( $language_titles[ $language_blog ] ) )
 					$display = $language_titles[ $language_blog ];
-					
-				} else $display = $languages[ $language_blog ];
+				else 
+					$display = $languages[ $language_blog ];
 				
 				$class = ( get_current_blog_id() == $language_blog ) ? 'id="mlp_current_locale"' : '';
 
@@ -120,19 +123,21 @@ if ( ! class_exists( 'mlp_widget' ) ) {
 				$post = ( ISSET( $linked_elements[ $language_blog ] ) ) ? get_blog_post( $language_blog, $linked_elements[ $language_blog ] ) : '';
 				
 				// Output link elements
-				echo '<li><a ' . $class . ' href="' . ( ( is_single() || is_page() ) && ISSET( $linked_elements[ $language_blog ] ) && 'publish' === $post->post_status ? get_blog_permalink( $language_blog, $linked_elements[ $language_blog ] ) : get_site_url( $language_blog ) ) . '">' . $display . '</a></li>';
+				echo '<li><a ' . $class . ' href="' . ( ( is_single() || is_page() ) && ISSET( $linked_elements[ $language_blog ] ) && 'publish' === $post->post_status ? get_blog_permalink( $language_blog, $linked_elements[ $language_blog ] ) : get_site_url( $language_blog ) ) . '?noredirect=' . $language_string . '">' . $display . '</a></li>';
 			}
 
 			echo '</ul>';
 
 			echo $after_widget;
 		}
+		
+		public function widget_register() {
+			register_widget( 'mlp_widget' );	
+		}
+		
+	} // Class END;
 
-	}
-
-	// Class END;
-	
 	// Initialize widget
-	add_action( 'widgets_init', create_function( '', 'return register_widget("mlp_widget");' ) );
+	add_action( 'widgets_init', array( 'Mlp_Widget', 'widget_register' ) );
 }
 ?>
